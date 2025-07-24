31bf435be707da7e8a5d4dcff5194458a4ddb498
@˚.˙ ᴋʜᴡᴀɴᴄʜᴀɴᴏᴋ 𖦹  ˙𓃠˚ <khwanchanoktafai@gmail.com>
2025-07-24 14:44:47 +0700

    Update and rename Variable-Operator.md to Variable-Operator.cpp

---
+++ Lab/lab4/Variable-Operator.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char employeeID[11];
+    int workinghrs = 0;
+    float Salary;
+
+    printf("Input the Employees ID(Max. 10 chars): ");
+    scanf("%s", employeeID);
+    printf("Input the working hrs: ");
+    scanf("%d", &workinghrs);
+    printf("Salary amount/hr:");
+    scanf("%f", &Salary);
+    printf("Employee ID: %s\n", employeeID);
+    printf("Salary = U$ %.2f\n", Salary * workinghrs);
+}
