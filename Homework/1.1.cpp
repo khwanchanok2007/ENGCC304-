ee602b581e8bfc7ba29ecfba3478a6f1061d09ef
@˚.˙ ᴋʜᴡᴀɴᴄʜᴀɴᴏᴋ 𖦹  ˙𓃠˚ <khwanchanoktafai@gmail.com>
2025-10-07 13:17:22 +0700

    Add files via upload

---
+++ Homework/1.1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int a,b,c;
+
+    printf("Test case: ");
+    scanf("%d %d %d",&a,&b,&c);
+
+    printf("Answer = ");
+    printf("%d",a+b+c);
+
+} //end main function
