5e8fe9a4d6b8262ab681f8fd64533eeabcbb1232
@˚.˙ ᴋʜᴡᴀɴᴄʜᴀɴᴏᴋ 𖦹  ˙𓃠˚ <khwanchanoktafai@gmail.com>
2025-10-07 15:20:39 +0700

    5.2.cpp

--- Homework/5.2.cpp
+++ Homework/5.2.cpp
@@ -5,26 +5,34 @@ int main()
     int score = 0;
     printf("Please enter your score: ");
     scanf("%d", &score);
-    printf("Grade: ");
-    if (score >= 80)
-    {
-        printf("A\n");
+    
+    if (score >= 80) {
+        printf("A!\n");
     }
-    else if (score >= 70 && score < 80)
-    {
-        printf("B\n");
+    else if (score >= 75 && score < 80) {
+        printf("B+!\n");
     }
-    else if (score >= 60 && score < 70)
-    {
-        printf("C\n");
+    else if (score >= 70 && score < 75) {
+        printf("B!\n");
     }
-    else if (score >= 50 && score < 60)
-    {
-        printf("D\n");
+    else if (score >= 65 && score < 70) {
+        printf("C+!\n");
+    }
+    else if (score >= 60 && score < 65) {
+        printf("C!\n");
+    }
+    else if (score >= 55 && score < 60) {
+        printf("D+!\n");
+    }
+    else if (score >= 50 && score < 55) {
+        printf("D!\n");
+    }
+    else if (score < 50) {
+        printf("F!\n");
     }
     else
     {
-        printf("F\n");
+        printf("please enter number only.");
     }
     return 0;
 }
